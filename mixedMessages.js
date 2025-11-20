4799e0de57745441e40a08a59c26b8ae83aaab23
JB_Ash <jashadu123@gmail.com>
2025-11-20 17:59:22 +0000

    Add simple html

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,30 +1,37 @@
-const greetings = ['Hi', 'Bonjour', 'Hola', 'Guten Tag', 'Ciao', 'Namaste', 'Ni hao', 'Konnichi wa']
-let a = Math.floor(Math.random() * greetings.length)
-let theGreeting = greetings[a]
-const countries = ['England', 'France', 'Spain', 'Germany', 'Italy', 'India', 'China', 'Japan']
-let theCountry = countries[a]
+const p = document.querySelector("p")
+const button = document.querySelector("button")
 
-const names = ['Amanda', 'Benny', 'Charlie', 'Dennis', 'Elijah', 'Finland', 'Gerald', 'Hassan', 'Ijenu', 'Jacob', 'Karl', 'Liam', 'Mary', 'Nana', 'Oscar', 'Piper', 'Quincy', 'Raymond', 'Steven', 'Tilly', 'Unice', 'Victor', 'Wapol', 'Xavier', 'Yewande', 'Zack']
-let b = Math.floor(Math.random() * names.length)
-let theName = names[b]
-let pNames = names.toSpliced(b, 1)
-//console.log(pNames)
-let c = Math.floor(Math.random() * pNames.length)
-let partner = pNames[c]
+button.addEventListener("click", () => {
+    const greetings = ['Hi', 'Bonjour', 'Hola', 'Guten Tag', 'Ciao', 'Namaste', 'Ni hao', 'Konnichi wa']
+    let a = Math.floor(Math.random() * greetings.length)
+    let theGreeting = greetings[a]
+    const countries = ['England', 'France', 'Spain', 'Germany', 'Italy', 'India', 'China', 'Japan']
+    let theCountry = countries[a]
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet']
-let theColor = colors[Math.floor(Math.random() * colors.length)]
+    const names = ['Amanda', 'Benny', 'Charlie', 'Dennis', 'Elijah', 'Finland', 'Gerald', 'Hassan', 'Ijenu', 'Jacob', 'Karl', 'Liam', 'Mary', 'Nana', 'Oscar', 'Piper', 'Quincy', 'Raymond', 'Steven', 'Tilly', 'Unice', 'Victor', 'Wapol', 'Xavier', 'Yewande', 'Zack']
+    let b = Math.floor(Math.random() * names.length)
+    let theName = names[b]
+    let pNames = names.toSpliced(b, 1)
+    //console.log(pNames)
+    let c = Math.floor(Math.random() * pNames.length)
+    let partner = pNames[c]
 
-const sports = ['football','basketball', 'tennis', 'table tennis', 'swimming', 'volleyball', 'netball', 'hockey', 'badminton']
-let theSport = sports[Math.floor(Math.random() * sports.length)]
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet']
+    let theColor = colors[Math.floor(Math.random() * colors.length)]
 
-const foods = ['spaghetti', 'fish n chips', 'pizza', 'jollof rice', 'fried rice', 'noodles', 'lasagne', 'tacos', 'fried chicken', 'sushi']
-let d = Math.floor(Math.random() * foods.length)
-let theFood = foods[d]
-let dFoods = foods.toSpliced(d, 1)
-//console.log(dFoods)
-let e = Math.floor(Math.random() * dFoods.length)
-let leastFav = dFoods[e]
+    const sports = ['football','basketball', 'tennis', 'table tennis', 'swimming', 'volleyball', 'netball', 'hockey', 'badminton']
+    let theSport = sports[Math.floor(Math.random() * sports.length)]
 
-const theMessage = `${theGreeting}! My name is ${theName} and I was born in ${theCountry}. My favourite color is ${theColor}. According to my partner ${partner}, the sport I am best at is ${theSport}. My favourite food is ${theFood} and my least favourite food is ${leastFav}.`
-console.log(theMessage)
+    const foods = ['spaghetti', 'fish n chips', 'pizza', 'jollof rice', 'fried rice', 'noodles', 'lasagne', 'tacos', 'fried chicken', 'sushi']
+    let d = Math.floor(Math.random() * foods.length)
+    let theFood = foods[d]
+    let dFoods = foods.toSpliced(d, 1)
+    //console.log(dFoods)
+    let e = Math.floor(Math.random() * dFoods.length)
+    let leastFav = dFoods[e]
+
+    const theMessage = `${theGreeting}! My name is ${theName} and I was born in ${theCountry}. My favourite color is ${theColor}. According to my partner ${partner}, the sport I am best at is ${theSport}. My favourite food is ${theFood} and my least favourite food is ${leastFav}.`
+    //console.log(theMessage)
+
+    p.innerText = theMessage
+})
